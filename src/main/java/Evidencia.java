af7fb2cb312b1b46659070ca1d135a4be342a3b4
Lean9090P <l.pino06@ufromail.cl>
2022-08-21 20:12:05 -0400

    Initial commit

---
+++ src/main/java/Evidencia.java
@@ -0,0 +1,8 @@
+public class Evidencia {
+    public static void main(String[] args) {
+        int i = 0;
+        for (int x = 1000; i <= x ;i++){
+            System.out.println(i + ".- Hola mundo");
+        }
+    }
+}
